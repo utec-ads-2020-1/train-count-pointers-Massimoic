373d61c3930a68b54d951620459bba8c8a119ebe
Massimo Imparato <massimo.imparato@utec.edu.pe>
2020-03-26 16:07:33 -0500

    Avance
    Sort y print del array de integers, falta el conteo de punteros

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_SIZE_PTRARRAY 512
+#define MAX_NUM_PTR 512
+#define MAX_NUM_INTVAR 512
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void count(int **p[]) {
+    sort(**p, **p + sizeof(**p));
+    for (int i = 0 ; i < sizeof(**p) ; i++) {
+        cout << **p[i] << ' ' << endl;
+    }
+}
+
+int main(){
+    int **ptrArray[MAX_SIZE_PTRARRAY] = {};
+    int  *intPtr[MAX_NUM_PTR];
+    int   intVar[MAX_NUM_INTVAR];
+
+    intVar[0] = 7;
+    intVar[1] = 6;
+    intVar[2] = 2;
+    intVar[3] = 9;
+    ptrArray[0] = &intPtr[0];
+    ptrArray[1] = &intPtr[1];
+    ptrArray[2] = &intPtr[2];
+    ptrArray[3] = &intPtr[3];
+    ptrArray[4] = NULL;
+    intPtr[0] = &intVar[0];
+    intPtr[1] = &intVar[1];
+    intPtr[2] = &intVar[2];
+    intPtr[3] = &intVar[3];
+
+    count(ptrArray);
+
     return 0;
 }
