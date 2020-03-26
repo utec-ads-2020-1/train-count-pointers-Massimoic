a5fc022d5447e63bc00f09dd082593c2fd36ec7a
Massimo Imparato <33756597+Massimoic@users.noreply.github.com>
2020-03-25 23:21:10 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/CountPointers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CountPointers.dir/main.cpp.obj"
+  "CountPointers.exe"
+  "CountPointers.exe.manifest"
+  "CountPointers.pdb"
+  "libCountPointers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CountPointers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
